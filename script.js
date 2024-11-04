1af40403f7a809d70ee5fe0c2a0622e616617005
arafatah <arafathowlader890@gmail.com>
2024-11-04 12:42:32 +0600

    understading contructor function

--- script.js
+++ script.js
@@ -51,7 +51,6 @@ console.log(john.species, foysal.species);
 console.log(john.hasOwnProperty('firstName'));
 console.log(john.hasOwnProperty('species'));
 
-
 /* Object Oriented Programming (OOP)  
 Coding Challenge #1 
 Your tasks: 
@@ -65,7 +64,41 @@ the new speed to the console
 4. Create 2 'Car' objects and experiment with calling 'accelerate' and 
 'brake' multiple times on each of them 
 Test data: 
+
+
 § Data car 1: 'BMW' going at 120 km/h 
 § Data car 2: 'Mercedes' going at 95 km/h 
 GOOD LUCK 
  */
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.make} is going at ${this.speed} km/h`);
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`${this.make} is going at ${this.speed} km/h`);
+};
+
+const bmw = new Car('BMW', 120);
+bmw.accelerate();
+bmw.brake();
+bmw.accelerate();
+bmw.accelerate();
+bmw.brake();
+
+const mercedes = new Car('Mercedes', 95);
+mercedes.accelerate();
+mercedes.brake();
+mercedes.accelerate();
+mercedes.accelerate();
+mercedes.brake();
+
+console.log(Object.prototype);
+console.log(bmw.__proto__.__proto__.__proto__);
